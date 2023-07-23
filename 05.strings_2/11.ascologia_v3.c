7731d439407214ede41e3d6dece1842bacdbff49
auank <kauanpablo089@gmail.com>
2023-07-23 18:13:40 -0300

    strings_2

--- 05.strings_2/11.ascologia_v3.c
+++ 05.strings_2/11.ascologia_v3.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
-int main(){
+int main() {
+    char name[100];
+    scanf("%[^\n]", name);
+
+    int sum = 0;
+    for (int i = 0; name[i] != '\0'; i++) sum += name[i];
     
+    int value = sum % 50;
+    printf("%d\n", value);
+    
+    int letter = 'a' - 1;
+    for (int i = 'a'; i <= 'z'; i++) {
+        int current_value = (sum + i) % 50;
+        if (current_value < value) {
+            letter = i;
+            value = current_value;
+        }
+    }
+
+    int new_value = (sum + letter) % 50;
+    if (letter != 'a' - 1) printf("%s%c\n%d\n", name, letter, new_value);
+    else printf("%s\n%d\n", name, value);
 }
